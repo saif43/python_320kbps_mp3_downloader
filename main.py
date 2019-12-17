a7e21193049519ce0d07527659f409da0ad3b1d6
saif43 <saif.ahmed.anik.0@gmail.com>
2019-12-17 13:00:04 +0600

    done

---
+++ main.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '320',
+    }],
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([input('Enter Youtube link: ')])
