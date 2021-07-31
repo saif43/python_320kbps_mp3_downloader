7bff9934f68ea8521ea71222242dda65d9ec5593
Saif Ahmed Anik <saif.ahmed@cg-bd.com>
2021-07-31 20:58:57 +0600

    ffmpeg download handled for Linux

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 from __future__ import unicode_literals
 import youtube_dl
+import os
+from sys import platform
+
+
+if platform == "linux" or platform == "linux2":
+    os.system("sudo apt install ffmpeg")
+    os.system("clear")
 
 
 ydl_opts = {
-    'format': 'bestaudio/best',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '320',
-    }],
+    "format": "bestaudio/best",
+    "postprocessors": [
+        {
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "mp3",
+            "preferredquality": "320",
+        }
+    ],
 }
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     while True:
-        print('[Type exit to close this application]')
-        x = input('Enter Youtube link: ')
+        print("[Type exit to close this application]")
+        x = input("Enter Youtube link: ")
 
-        if x == 'exit':
+        if x == "exit":
             break
 
-        ydl.download([x])
+        try:
+            ydl.download([x])
+        except:
+            os.system("clear")
+            print("Please enter correct youtube URL or enter exit\n")
